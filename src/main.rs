dfac723eb8113e1af6d57bb09c668a82f1e11799
Chi Ting, Low <ctlow83@gmail.com>
2023-02-04 22:36:15 +0800

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+// import library
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number");
+
+    // generate random number from 1 to 100
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Please input your guess.");
+
+        //create new string to store value that is mutable
+        let mut guess = String::new();
+
+        // received input of the string and except to throw exception
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // parse the number convert from string to int
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
